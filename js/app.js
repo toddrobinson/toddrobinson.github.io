9953b32f33947c95603d659b3f24415a66572f72
Todd Robinson <toddsinclairrobinson@gmail.com>
2015-10-09 16:03:31 -0400

    Add animation function and start animate at load.

--- js/app.js
+++ js/app.js
@@ -37,3 +37,23 @@ app.config(function($routeProvider){
 $("ul.nav li a ").on("click" , function(){
   $('body').scrollTo('.contentPane', 800);
 });
+
+$(document).ready(function(){
+  animate($('header > *'), "bounceInDown");
+});
+
+
+/*
+Using the animation.css file by Daniel Eden
+Takes in element to perform the animation on, and the motion to do as a string.
+Executes the animation by adding the two associated classes, and then after the aniamtion is over, removes the classes.
+
+*/
+function animate(element, action) {
+  var animationName = 'animated ' + action;
+  var atEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+  $(element).addClass(animationName).one(atEnd,function(){
+    $(element).removeClass(animationName);
+  });
+}
